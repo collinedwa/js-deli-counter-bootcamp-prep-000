511766117c773b929bc1d95d653fd4b1438f6a33
Collin Edwards <collinedwards1579@gmail.com>
2018-01-22 21:26:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = []
 
 function takeANumber(katzDeliLine, p){
   katzDeliLine.push(p)
-  return ("Welcome, " + p + ". You're currently number " + katzDeliLine.length + " in line.")
+  return ("Welcome, " + p + ". You are number " + katzDeliLine.length + " in line.")
 }
 
 function nowServing(katzDeliLine, i){
