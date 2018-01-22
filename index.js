d605ff8938740418fc62884e2dc9b90345e20fc6
Collin Edwards <collinedwards1579@gmail.com>
2018-01-22 21:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ function takeANumber(katzDeliLine, p){
   return ("Welcome, " + p + ". You are number " + katzDeliLine.length + " in line.")
 }
 
-function nowServing(katzDeliLine, i){
+function nowServing(katzDeliLine){
   if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
   }
   else{
     return ("Currently serving " + katzDeliLine.shift() + ".")
+  }
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }
+  else{
+    return ("The line is currently " + 
   }
 }
