1325d3a06fb0ecc1f44292905fdd3fb9f9ef996d
Collin Edwards <collinedwards1579@gmail.com>
2018-01-22 21:18:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine, p){
+  
+}
+
+function nowServing(katzDeliLine, i){
+  for(i = katzDeliLine.length; i >= 1;){
+    katzDeliLine[0]
+    katzDeliLine.shift()
+  }
+  return "The line is currently empty."
+}
