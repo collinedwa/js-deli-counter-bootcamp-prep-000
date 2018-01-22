dfc62c1051bd949cbbe046fd855e1c9ee5ce8bc5
Collin Edwards <collinedwards1579@gmail.com>
2018-01-22 21:41:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(katzDeliLine){
   else{
     var lineNames = []
     for(var n = 0; n < katzDeliLine.length; n++){
-      lineNames.push(n + 1 + ". " + katzDeliLine[n] + " ")
+      lineNames.push(n + 1 + ". " + katzDeliLine[n])
     }
     return ("The line is currently: " + lineNames)
   }
